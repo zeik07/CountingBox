ba1a1242e0878904be20f261b236b982df7b8be7
Kevin <klgvette07@gmail.com>
2018-12-31 03:15:57 -0500

    slight refactor, added min/max words/chars

--- CountingBox/MainWindow.xaml.cs
+++ CountingBox/MainWindow.xaml.cs
@@ -27,14 +27,79 @@ namespace CountingBox
 
         private void TextToCount_TextChanged(object sender, TextChangedEventArgs e)
         {
-            int textCount = 0;
             string text = TextToCount.Text;
-            textCount = text.Length;
-            CharCount.Content = textCount;
+            int minChars = new int();
+            int maxChars = new int();
+            int minWords = new int();
+            int maxWords = new int();
+            
+            if (MinChars.Text == "")
+            {
+                minChars = 0;
+            }
+            else
+            {
+                minChars = Convert.ToInt32(MinChars.Text);
+            }
+
+            if (MaxChars.Text == "")
+            {
+                maxChars = 0;
+            }
+            else
+            {
+                maxChars = Convert.ToInt32(MaxChars.Text);
+            }
+
+            if (MinWords.Text == "")
+            {
+                minWords = 0;
+            }
+            else
+            {
+                minWords = Convert.ToInt32(MinWords.Text);
+            }
+
+            if (MaxWords.Text == "")
+            {
+                maxWords = 0;
+            }
+            else
+            {
+                maxWords = Convert.ToInt32(MaxWords.Text);
+            }
+
+            int charsTotal = Chars(text);
+            CharsLimits(charsTotal, minChars, maxChars);
+            CharCount.Content = charsTotal;
+
             int spacesTotal = Spaces(text);
             SpaceCount.Content = spacesTotal;
+
             int wordsTotal = Words(text);
+            WordsLimits(wordsTotal, minWords, maxWords);
             WordCount.Content = wordsTotal;
+        }
+
+        private int Chars(string textString)
+        {
+            return textString.Length;
+        }
+
+        private void CharsLimits(int total, int min, int max)
+        {
+            if (max != 0 && total > max)
+            {
+                CharCount.Foreground = Brushes.Red;
+            }
+            else if (min != 0 && total < min)
+            {
+                CharCount.Foreground = Brushes.Red;
+            }
+            else
+            {
+                CharCount.Foreground = Brushes.Black;
+            }
         }
 
         private int Spaces(string textString)
@@ -62,6 +127,22 @@ namespace CountingBox
                 }
             }
             return wordsTotal;
+        }
+
+        private void WordsLimits(int total, int min, int max)
+        {
+            if (max != 0 && total > max)
+            {
+                WordCount.Foreground = Brushes.Red;
+            }
+            else if (min != 0 && total < min)
+            {
+                WordCount.Foreground = Brushes.Red;
+            }
+            else
+            {
+                WordCount.Foreground = Brushes.Black;
+            }
         }
     }
 }
